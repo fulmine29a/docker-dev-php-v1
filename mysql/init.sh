7f7351ffa51200fda571f1b18177f690ff5daa7a
fulmine <fulmine29a@gmail.com>
2018-02-08 21:29:41 +0300

    1. convert to library 2. mysql dump auto load

--- mysql/init.sh
+++ mysql/init.sh
@@ -1,4 +1,18 @@
-#/bin/sh
+#!/usr/bin/env bash
 
-sudo -u dev mysqld
-mysql < /root/init.sql
+service mysql start
+
+if [ -f /root/needinit ]; then
+
+    echo 'init.. please wait';
+
+    mysql < /root/init.sql
+
+    if [ -f /dump/dump.sql ]; then
+        mysql < /dump/dump.sql
+    fi
+
+    rm -f /root/needinit
+fi
+
+echo 'ready';
--- site/public_html/q.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: fulmine
- * Date: 29.09.17
- * Time: 23:51
- */
-phpinfo();
